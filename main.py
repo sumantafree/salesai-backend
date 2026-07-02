a493ee08731869ff02dec502d6b6964bf3a8a0ef
sumantafree <mindartcollection@gmail.com>
2026-07-02 12:50:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "https://sales.digitalsumanta.com",
-]
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
